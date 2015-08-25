9b2e82592fcbf8460a92e98d38f701dbcf75ba9c
Patrick Perini <pcperini@gmail.com>
2015-08-24 22:25:12 -0700

    lol

--- mystery_box/pick_source.py
+++ mystery_box/pick_source.py
@@ -7,9 +7,8 @@ import random
 
 # Main
 if __name__ == "__main__":
-#    source = Source()
-#    content = Content(source.url)
-    content = Content("http://www.huffingtonpost.com/2015/08/20/yolo-joe-11-reasons-why-you-should-jump-in-already_n_8030182.html")
+    source = Source()
+    content = Content(source.url)
     content.save()
 
     content.push()
